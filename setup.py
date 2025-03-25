cd0f6c1bc7454190f857f5e595e4022adc4806ef
AbhinavS-30 <abhinav.302001@gmail.com>
2025-03-25 19:00:39 -0400

    Finished

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'Information Retrieval System',
+    version = '0.0.0',
+    author = 'Abhinav Sharma',
+    author_email= 'abhinav.302001@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
---
+++ src/helper.py
@@ -0,0 +1,44 @@
+import os
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.llms import Ollama
+from langchain_community.vectorstores import FAISS
+from langchain.chains import ConversationalRetrievalChain
+from langchain.memory import ConversationBufferMemory
+from dotenv import load_dotenv
+
+load_dotenv()
+GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
+os.environ['GOOGLE_API_KEY'] = GOOGLE_API_KEY
+
+
+def get_pdf_text(pdf_docs):
+    text = ''
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            extracted = page.extract_text()
+            if extracted:
+                text += extracted
+    return text
+
+def get_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+def get_vector_store(text_chunks):
+    embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
+    return vector_store
+
+def get_conversational_chain(vector_store):
+    llm = Ollama(model="llama3")  # Or mistral, gemma, etc.
+    memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
+    conversational_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vector_store.as_retriever(),
+        memory=memory
+    )
+    return conversational_chain
